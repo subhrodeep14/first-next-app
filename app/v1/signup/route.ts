201a5971b25d1c2780c9208992e78bed12e0710f
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2024-12-28 01:45:05 +0530

    database connected

--- app/v1/signup/route.ts
+++ app/v1/signup/route.ts
@@ -1,9 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma=new PrismaClient();
 
 export async function POST(req:NextRequest,res:NextResponse) {
   
+  
     const data=await req.json();
     console.log(data);
+    const user=await prisma.user.create({
+        data:{
+            name:data.username,
+            password:data.password,
+            email:data.email
+        }
+    });
 
     return NextResponse.json({ message: "Sign up" });
 }
